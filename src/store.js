d04d31026ef17000df8743fc49c147965f501fd8
noeyeyh <ahy020606@naver.com>
2023-09-05 02:04:50 +0900

    장바구니 수정

--- src/store.js
+++ src/store.js
@@ -1,19 +1,23 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 import user from './store/userSlice.js';
 
-
 let Bag = createSlice({
     name: 'Bag',
-    initialState : [
-        
-    ],
-    reducers : {
-        addCount(state, action){
-            let 번호 = state.findIndex((a) => {return a.id === action.payload})
-            state[번호].count++
+    initialState: [],
+    reducers: {
+        addCount(state, action) {
+            let 번호 = state.findIndex((a) => a.id === action.payload);
+            if (번호 !== -1) {
+                state[번호].count++;
+            }
         },
         addItem(state, action) {
-            state.push(action.payload)
+            let 번호 = state.findIndex((a) => a.id === action.payload.id);
+            if (번호 !== -1) {
+                state[번호].count++;
+            } else {
+                state.push({ ...action.payload, count: 1 });
+            }
         },
         clearBag(state) {
             state.length = 0; // 장바구니 비우기
